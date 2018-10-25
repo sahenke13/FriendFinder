5832f691cb83e860758a72c15621bab4229a65b3
sahenke13 <shenke13@gmail.com>
2018-10-25 13:50:55 -0500

    Testing out herok deployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+//comment here
 
 require(path.join(__dirname,'./app/routing/apiRoutes.js'))(app);
 require(path.join(__dirname, '/app/routing/htmlRoutes.js'))(app);
